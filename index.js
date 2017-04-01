e7b66f14f1dd77706cd29ef3d04cad9bbe593574
Your Name <you@example.com>
2017-04-01 14:33:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Code React element here
+
+
+
+
+const meInReact = React.createElement('div' {className: "me"},
+    React.createElement('h1', {}, 'An Awesome Person');
+    React.createElement('p',{},"Who is learning React")
+    React.createElement('ul',{className: 'me_interests'},
+        [
+          React.createElement('li',{},"JAvaScript")
+          React.createElement('li',{},"React")
+          React.createElement('li',{},"Movies ")
+          React.createElement('li',{},"ICe Cream")
+        ]
+    ))
+
+ReactDOM.render(meInReact, document.getElementById('main'))
